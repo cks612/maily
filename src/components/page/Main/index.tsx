63fadf5cb60b61780a65fcd25049450437a3eb2f
cks612 <kyusung612@gmail.com>
2023-01-12 01:26:20 +0900

    [ADD] search input added to the mainPage & fontawesome package installed
    검색 input UI 배치 구현 및 fontawesome 패키지 설치

--- src/components/page/Main/index.tsx
+++ src/components/page/Main/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const MainPage = () => {
   return (
     <StyledContainer>
+      {/* component */}
       <StyledMainVideo>
         <StyledVideoTitle>
           <h2>영화로 채우는 하루의 노곤함</h2>
@@ -13,7 +16,26 @@ const MainPage = () => {
         <video autoPlay muted loop playsInline width="100%">
           <source src="/video/mainVideo.mp4" type="video/mp4" />
         </video>
+
+        {/* component */}
+        <InputWrapper>
+          <InputContainer>
+            <SearchInput placeholder="영화 찾기" />
+            <FontAwesomeIcon className="spinner" icon={faSearch} size="2x" />
+          </InputContainer>
+        </InputWrapper>
       </StyledMainVideo>
+
+      <h2>main</h2>
+      <h2>main</h2>
+      <h2>main</h2>
+      <h2>main</h2>
+      <h2>main</h2>
+      <h2>main</h2>
+      <h2>main</h2>
+      <h2>main</h2>
+      <h2>main</h2>
+      <h2>main</h2>
       <h2>main</h2>
     </StyledContainer>
   );
@@ -30,16 +52,20 @@ const StyledContainer = styled.div`
 `;
 
 const StyledMainVideo = styled.div`
+  position: relative;
   width: 100%;
   height: 500px;
+  overflow: hidden;
 `;
 
 const StyledVideoTitle = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
+  width: 100%;
+  top: 35%;
+  line-height: 40px;
   color: #fff;
   animation: smoothAppear 5s;
 
@@ -59,4 +85,42 @@ const StyledVideoTitle = styled.div`
       opacity: 1;
     }
   }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  bottom: 100px;
+  animation: smoothAppear 5s;
+
+  @keyframes smoothAppear {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 50%;
+  padding: 20px 20px;
+  border-radius: 15px;
+  background: #fff;
+  color: #000;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  background: transparent;
+  outline: none;
+  border: none;
+  color: #000;
+  font-size: 1.3em;
 `;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/common/Layout";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
