4223065f466c9202d4237af2cb36c5b512b54bae
cks612 <kyusung612@gmail.com>
2023-01-13 01:38:25 +0900

    [ADD] react-slick library added
    react-slick 라이브러리로 일일 박스 오피스 영화 정보 무한 캐러셀 구현

--- src/components/Movie/DailyMovie/index.tsx
+++ src/components/Movie/DailyMovie/index.tsx
@@ -4,30 +4,47 @@ import { MovieDataProps } from "../../../pages/api/movie";
 import ContentTitle from "../../Content/ContentTitle";
 import ContentWrapper from "../../Content/ContentWrapper";
 import MovieCard from "../MovieCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface DataProps {
   items: MovieDataProps[];
 }
+
 const Card = ({ items }: DataProps) => {
+  const settings = {
+    infinite: true,
+    arrows: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    autoplay: true,
+    speed: 10000,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+  };
   return (
     <ContentWrapper>
       <ContentTitle>일일 박스 오피스</ContentTitle>
 
       <StyledDailyMovie>
-        {items?.map((item: MovieDataProps) => {
-          const { movieNm, subtitle, userRating, link, image, rnum } = item;
-          return (
-            <MovieCard
-              key={rnum}
-              rnum={rnum}
-              movieNm={movieNm}
-              subtitle={subtitle}
-              userRating={userRating}
-              link={link}
-              image={image}
-            />
-          );
-        })}
+        <StyledSlider {...settings}>
+          {items?.map((item: MovieDataProps) => {
+            const { movieNm, subtitle, userRating, link, image, rnum } = item;
+            return (
+              <MovieCard
+                key={rnum}
+                rnum={rnum}
+                movieNm={movieNm}
+                subtitle={subtitle}
+                userRating={userRating}
+                link={link}
+                image={image}
+              />
+            );
+          })}
+        </StyledSlider>
       </StyledDailyMovie>
     </ContentWrapper>
   );
@@ -36,8 +53,15 @@ const Card = ({ items }: DataProps) => {
 export default Card;
 
 const StyledDailyMovie = styled.div`
-  display: -webkit-box;
-  gap: 50px;
+  height: 450px;
 `;
 
-// poster, title, rating, subtitle
+const StyledSlider = styled(Slider)`
+  .slick-slide {
+    padding: 10px 5px;
+    margin: 0 20px;
+  }
+  .slick-list {
+    margin: 0 -20px;
+  }
+`;
